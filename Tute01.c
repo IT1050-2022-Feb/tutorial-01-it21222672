f6ca2952267f144b9ff91cdae99461e1f11738fe
it21222672 <99347614+it21222672@users.noreply.github.com>
2022-02-09 21:55:12 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,16 @@
 #include <stdio.h>
 
 int main() {
+   int mark1 , mark2;
+   float ave;
+   printf("enter mark1");
+   scanf("%d" ,&mark1);
+   printf("enter mark2");
+   scanf("%d" ,&mark2);
+   
+   
+   ave = (mark1 + mark2)/2;
+   printf("%f" ,ave);
   
   return 0;
 }
